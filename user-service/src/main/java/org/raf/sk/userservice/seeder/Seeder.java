236f09aec878d7dbb15858a28491a49a0d646dec
Vasilije Jukic <vasilijejukic@gmail.com>
2023-12-18 17:43:48 +0100

    Created tests

--- user-service/src/main/java/org/raf/sk/userservice/seeder/Seeder.java
+++ user-service/src/main/java/org/raf/sk/userservice/seeder/Seeder.java
@@ -9,7 +9,9 @@ import org.raf.sk.userservice.repository.RoleRepository;
 import org.raf.sk.userservice.repository.StatusRepository;
 import org.raf.sk.userservice.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.sql.Date;
@@ -18,24 +20,26 @@ import java.sql.Date;
 @Profile({"default"})
 @Component
 @AllArgsConstructor
+@ConditionalOnProperty(name = "seed.data", havingValue = "true")
 public class Seeder implements CommandLineRunner {
 
+    private final BCryptPasswordEncoder encoder;
     private RoleRepository roleRepository;
     private StatusRepository statusRepository;
     private UserRepository userRepository;
 
     @Override
     public void run(String... args) {
-        Role roleAdmin = new Role(0L, "ADMIN");
-        Role roleManager = new Role(1L, "MANAGER");
-        Role roleClient = new Role(2L, "CLIENT");
+        Role roleAdmin = new Role(1L, "ADMIN");
+        Role roleManager = new Role(2L, "MANAGER");
+        Role roleClient = new Role(3L, "CLIENT");
         roleAdmin = roleRepository.save(roleAdmin);
         roleManager = roleRepository.save(roleManager);
         roleClient = roleRepository.save(roleClient);
 
-        Status statusActive = new Status(0L, "VERIFIED");
-        Status statusBanned = new Status(1L, "UNVERIFIED");
-        Status statusDeleted = new Status(2L, "BANNED");
+        Status statusActive = new Status(1L, "VERIFIED");
+        Status statusBanned = new Status(2L, "UNVERIFIED");
+        Status statusDeleted = new Status(3L, "BANNED");
         statusActive = statusRepository.save(statusActive);
         statusRepository.save(statusBanned);
         statusRepository.save(statusDeleted);
@@ -46,14 +50,14 @@ public class Seeder implements CommandLineRunner {
     }
 
     private void insertAdmins(Role roleAdmin, Status statusActive) {
-        User admin = new User("admin", "admin", "admin", "admin", "admin@gmail.com", Date.valueOf("1990-10-10"));
+        User admin = new User("admin", encoder.encode("admin"), "admin", "admin", "admin@gmail.com", Date.valueOf("1990-10-10"));
         admin.setUserRole(roleAdmin);
         admin.setUserStatus(statusActive);
         userRepository.save(admin);
     }
 
     private void insertManagers(Role roleManager, Status statusActive) {
-        User manager1 = new User("m1", "m1", "m1", "m1", "manager1@gmail.com", Date.valueOf("1990-10-10"));
+        User manager1 = new User("m1", encoder.encode("m1"), "m1", "m1", "manager1@gmail.com", Date.valueOf("1990-10-10"));
         manager1.setHall("Gym 1");
         manager1.setHireDate(Date.valueOf("2022-11-10"));
         manager1.setUserRole(roleManager);
@@ -62,7 +66,7 @@ public class Seeder implements CommandLineRunner {
     }
 
     private void insertClients(Role roleClient, Status statusActive) {
-        User client1 = new User("c1", "c1", "c1", "c1", "theBestEmail@gmail.com", Date.valueOf("2001-11-3"));
+        User client1 = new User("c1", encoder.encode("c1"), "c1", "c1", "theBestEmail@gmail.com", Date.valueOf("2001-11-3"));
         client1.setLicenseID("7491412");
         client1.setTotalSessions(0);
         client1.setUserRole(roleClient);
