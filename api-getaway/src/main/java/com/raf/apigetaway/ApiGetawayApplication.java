5e618ce3ec5736f35e19add3105333649f4d9d98
Vasilije Jukic <vasilijejukic@gmail.com>
2023-12-26 13:10:16 +0100

    Getaway and eureka implementation

---
+++ api-getaway/src/main/java/com/raf/apigetaway/ApiGetawayApplication.java
@@ -0,0 +1,34 @@
+package com.raf.apigetaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGetawayApplication {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetawayApplication.class, args);
+    }
+
+}
